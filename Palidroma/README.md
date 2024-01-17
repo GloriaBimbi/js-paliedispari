3a923ee3887de41f2ea74fb831c9f524208cfa19
GloriaBimbi <gloria.bimbi1@libero.it>
2024-01-17 22:04:05 +0100

    feat exercise

---
+++ Palidroma/README.md
@@ -0,0 +1,11 @@
+# PAROLA PALINDROMA
+
+## SVOLGIMENTO:
+
+- chiedo una parola all'utente
+- trasformo la parola data in un array che contiene come elementi le lettere che la compongono
+- creo una funzione per capire se la parola inserita è palindroma
+  - dichiaro una variabile in cui raccoglierò la parola ribaltata
+  - con un ciclo for ribalto la parola inserita dall'utente
+  - con in if capisco se la parola dell'utente e il suo contrario sono uguali e quindi se essa è palindroma
+- invoco la funzione
--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -1,5 +1,5 @@
 // chiedo una parola all'utente
-const userWord = prompt("inserisci una parola");
+const userWord = prompt("inserisci una parola").toLowerCase();
 console.log("La parola inserita dall'utente è: " + userWord);
 
 // trasformo la parola data in un array che contiene come elementi le lettere che la compongono
@@ -11,19 +11,21 @@ function palindromWord() {
   // dichiaro una variabile in cui raccoglierò la parola ribaltata
   let reverseWord = "";
   // con un ciclo for ribalto la parola inserita dall'utente
-  for (let i = userWordsArray.lenght - 1; i >= 0; i--) {
+  for (let i = userWordsArray.length - 1; i >= 0; i--) {
     reverseWord += userWordsArray[i];
-    console.log(reverseWord);
-    console.log(i);
+    console.log("Al giro " + i, "la parola è: " + reverseWord);
   }
   console.log("La parola inserita dall'utente, al contrario è: " + reverseWord);
 
   // con in if capisco se la parola dell'utente e il suo contrario sono uguali e quindi se essa è palindroma
   if (userWord == reverseWord) {
     alert("La parola inserita è PALINDROMA");
+    console.log(`${userWord} e ${reverseWord} sono palindrome`);
   } else {
     alert("La parola inserita NON è palindroma");
+    console.log(`${userWord} e ${reverseWord} NON sono palindrome`);
   }
 }
 
+// invoco la funzione
 palindromWord();
